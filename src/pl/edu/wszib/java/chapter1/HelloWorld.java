61caa8ac03070f4d9e3c17902eb4ac6635c8e1f6
Marcin <marcin@softem.pl>
2017-10-13 20:15:04 +0200

    Initial commit

---
+++ src/pl/edu/wszib/java/chapter1/HelloWorld.java
@@ -0,0 +1,7 @@
+package pl.edu.wszib.java.chapter1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/pl/edu/wszib/java/chapter2/demo1/MarsApplication.java
@@ -0,0 +1,21 @@
+package pl.edu.wszib.java.chapter2.demo1;
+
+class MarsApplication {
+    public static void main(String[] arguments) {
+        MarsRobot spirit = new MarsRobot();
+        spirit.status = "eksploracja";
+        spirit.speed = 2;
+        spirit.temperature = -60;
+
+        spirit.showAttributes();
+        System.out.println("Zwiększanie prędkości do 3.");
+        spirit.speed = 3;
+        spirit.showAttributes();
+        System.out.println("Zmiana temperatury na -90.");
+        spirit.temperature = -90;
+        spirit.showAttributes();
+        System.out.println("Sprawdzenie temperatury.");
+        spirit.checkTemperature();
+        spirit.showAttributes();
+    }
+}
