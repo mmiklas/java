5bfb8d3d17f73b699d9886876db4baf9cdb14919
Marcin <marcin@softem.pl>
2017-10-14 12:32:00 +0200

    Podstawy programowania w języku Java

---
+++ src/chapter2/MarsApplication2.java
@@ -0,0 +1,29 @@
+package chapter2;
+
+class MarsApplication2 {
+    public static void main(String[] arguments) {
+        MarsRobot spirit = new MarsRobot();
+        spirit.status = "eksploracja";
+        spirit.speed = 2;
+        spirit.temperature = -60;
+
+        spirit.showAttributes();
+        System.out.println("Zwiększanie prędkości do 3.");
+        spirit.speed = 3;
+        spirit.showAttributes();
+        System.out.println("Zmiana temperatury na -90.");
+        spirit.temperature = -90;
+        spirit.showAttributes();
+        System.out.println("Sprawdzenie temperatury.");
+        spirit.checkTemperature();
+        spirit.showAttributes();
+
+        //Lab2.1
+        MarsRobot opportunity = new MarsRobot();
+        System.out.println("\nOkazja::");
+        opportunity.status = "eksploracja";
+        opportunity.speed = 4;
+        opportunity.temperature = -90;
+        opportunity.showAttributes();
+    }
+}
