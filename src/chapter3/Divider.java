84eb22ea7fecaedd23a5390cc8d0d7488a6d7d15
Marcin <marcin@softem.pl>
2017-10-18 12:14:43 +0200

    Prace nad chapter 3

---
+++ src/chapter3/Divider.java
@@ -0,0 +1,15 @@
+/* Rozwiązanie dla rozdział 2., ćwiczenie 2. */
+   
+package chapter3;
+
+class Divider {
+    public static void main(String[] arguments) {
+        float number1 = 15;
+        float number2 = 6;
+        float result = number1 / number2;
+        float remainder = number1 % number2;
+        System.out.println(number1 + " dzielone przez " + number2);
+        System.out.println("\nWynik\tReszta");
+        System.out.println(result + "\t" + remainder);
+    }
+}
