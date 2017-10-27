09b40c72e1a0924b3a13e1f8d0c902e2b5ef6810
Marcin <marcin@softem.pl>
2017-10-27 19:11:49 +0200

    Automatyzacja Jekyll

---
+++ src/chapter4/InstanceCounter.java
@@ -0,0 +1,27 @@
+package chapter4;
+
+public class InstanceCounter {
+    private static int numInstances = 0;
+
+    protected static int getCount() {
+        return numInstances;
+    }
+
+    private static void addInstance() {
+        numInstances++;
+    }
+
+    InstanceCounter() {
+        InstanceCounter.addInstance();
+    }
+
+    public static void main(String[] arguments) {
+        System.out.println("Rozpoczynanie z " +
+            InstanceCounter.getCount() + " obiektów");
+        for (int  i = 0; i < 500; ++i) {
+            new InstanceCounter();
+        }
+        System.out.println("Utworzono " +
+            InstanceCounter.getCount() + " obiektów");
+    }
+}
