de4a5224090b27f2a0dddb78dbfaba9119cf4bd0
Marcin <marcin@softem.pl>
2017-11-04 20:45:18 +0100

    Poprawki

---
+++ src/results/OutOfMemory.java
@@ -0,0 +1,19 @@
+package results;
+
+public class OutOfMemory {
+    public static void main(String[] args) {
+        int i = 10000000;
+        int j = 10000000;
+        int k = 10000000;
+
+        Long[][][] l = new Long[i][j][k];
+        Long ll = 999999999999999999L;
+        for (int a=1; j<i ;a++) {
+            for (int b = 0; b <j ; b++) {
+                for (int c = 0; c <k ; c++) {
+                    l[a][b][c]=ll;
+                }
+            }
+        }
+    }
+}
