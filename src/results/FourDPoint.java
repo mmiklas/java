5671ec05c730aae89376b1b6a4fef0c068ad8159
Marcin <marcin@softem.pl>
2017-11-04 21:21:49 +0100

    Poprawki

---
+++ src/results/FourDPoint.java
@@ -0,0 +1,22 @@
+package results;
+
+import java.awt.Point;
+ 
+class FourDPoint extends Point {
+    int z;
+    int t;
+
+    FourDPoint(int x, int y, int inZ, int inT) {
+        super(x,y);
+        this.z = inZ;
+        this.t = inT;
+    }
+
+    public static void main(String[] arguments) {
+        FourDPoint np = new FourDPoint(5, 5, 4, 100);
+        System.out.println("x wynosi " + np.x);
+        System.out.println("y wynosi " + np.y);
+        System.out.println("z wynosi " + np.z);
+        System.out.println("t wynosi " + np.t);
+    }
+}
