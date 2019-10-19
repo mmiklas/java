a3c57cf0d658239b24c07ab6ca46336366e72277
Marcin Mikłas <miki@wszib.edu.pl>
2019-10-19 10:27:32 +0200

    Poprawki

--- 7_instrukcje_sterujace_i_petle/src/lab/Matrix.java
+++ 7_instrukcje_sterujace_i_petle/src/lab/Matrix.java
@@ -4,6 +4,9 @@ public class Matrix {
     public static void main(String[] args) {
         int n = 4;
 
+        System.out.println("n = " + n + " matrix");
+        System.out.println("------------");
+
         for (int i = 0; i < n; i++) {
             for (int j = 1; j <= n; j++) {
                 System.out.printf("%3s", j + i + " ");
