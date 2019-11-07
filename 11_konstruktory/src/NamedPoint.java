901d2ac03690d5853ed62a140540980053be5f01
Marcin Mikłas <miki@wszib.edu.pl>
2019-11-07 18:45:30 +0100

    Poprawki

---
+++ 11_konstruktory/src/NamedPoint.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+class NamedPoint extends Point {
+    String name;
+
+    NamedPoint(int x, int y, String name) {
+        super(x, y);
+        this.name = name;
+    }
+
+    public static void main(String[] arguments) {
+        NamedPoint np = new NamedPoint(5, 5, "SmallPoint");
+        System.out.println("x wynosi " + np.x);
+        System.out.println("y wynosi " + np.y);
+        System.out.println("name wynosi " + np.name);
+    }
+}
