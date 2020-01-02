f2a6aaff48b993f466c8c7e4ec77ec05b0d1d464
Marcin Mikłas <miki@wszib.edu.pl>
2020-01-02 21:00:24 +0100

    Rozwiazania zdan domowych

---
+++ dodatki/src/Zoo.java
@@ -0,0 +1,20 @@
+package results;
+
+public class Zoo {
+    public static void main(String[] args) {
+        Animal cat = new Cat();
+        Animal dog = new Dog();
+    }
+}
+
+class Animal {
+}
+
+interface Voice {
+}
+
+class Cat extends Animal implements Voice {
+}
+
+class Dog extends Animal implements Voice {
+}
