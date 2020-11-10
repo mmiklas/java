46e5935d180b9a42fe667756c273b828b977314f
Marcin Mikłas <miki@wszib.edu.pl>
2020-11-10 14:54:35 +0100

    Nowy rozdział

---
+++ dodatki/src/_NumberSystemOperation.java
@@ -0,0 +1,15 @@
+public class _NumberSystemOperation {
+
+    public static void main(String[] args) {
+
+        int a = 10; //dziesiętnie
+        int b = 010; //ósemkowo
+        int c = 0x10; //szestnastkowo
+
+        int d = a + b -c;
+
+        System.out.println(a + " + " + b + " - " + c + " = " + d);
+
+        System.out.println(d + " -> "+ Integer.toBinaryString(d));
+    }
+}
